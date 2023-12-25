7b86796b6dfab857d3248fadb6f2e7a32d2a2e6c
Evgenii Sopov <mrseakg@gmail.com>
2023-12-26 01:20:46 +0700

    Added template

--- template/index.js
+++ template/index.js
@@ -40,8 +40,6 @@ function hide_screen_loading() {
     document.getElementById("screen_loading").style.display = "none";
 }
 
-
-
 function loading_resources() {
     console.warn("Simulation loading resources... TODO");
     getJSON("./game.json", function(st, result) {
@@ -75,6 +73,70 @@ document.addEventListener("DOMContentLoaded", (event) => {
     loading_resources();
 });
 
+function onclick_next_scene(el) {
+    var sceneid = el.srcElement.getAttribute("next_scene_id");
+    next_scene(sceneid);
+}
+
+function onclick_next_scene_btn(el) {
+    var sceneid = el.getAttribute("next_scene_id");
+    next_scene(sceneid);
+}
+
+function escapeHtml(html){
+    var text = document.createTextNode(html);
+    var p = document.createElement('p');
+    p.appendChild(text);
+    return p.innerHTML;
+}
+
+function next_scene(sceneid) {
+    console.log("next_scene ", sceneid)
+    var scene = window.gameJson["scenes"][sceneid];
+    document.getElementById("scene_background_back").style["background-image"] = "url(\"" + scene["background"] + "\")";
+    document.getElementById("scene_background_front").style["background-image"] = "url(\"" + scene["background"] + "\")";
+    document.getElementById("scene_text").innerHTML = scene["text"];
+    document.getElementById("scene_text_panel").onclick = null;
+    document.getElementById("scene_background_front").onclick = null;
+    document.getElementById("scene_text_panel").setAttribute("next_scene_id", "");
+    document.getElementById("scene_background_front").setAttribute("next_scene_id", "");
+    document.getElementById("scene_buttons_panel").style.display = "none";
+    document.getElementById("scene_buttons_content").innerHTML = "";
+
+    if (scene["next"].length == 1) {
+        document.getElementById("scene_text_panel").setAttribute("next_scene_id", scene["next"][0]["id"]);
+        document.getElementById("scene_background_front").setAttribute("next_scene_id", scene["next"][0]["id"]);
+
+        document.getElementById("scene_text_panel").onclick = onclick_next_scene;
+        document.getElementById("scene_background_front").onclick = onclick_next_scene;
+    } else {
+        document.getElementById("scene_buttons_panel").style.display = "";
+        for (var i in scene["next"]) {
+            var btn_text = scene["next"][i]["text"];
+            var btn_id = scene["next"][i]["id"];
+
+            document.getElementById("scene_buttons_content").innerHTML += ""
+                + "<div class='scene-button-choise' "
+                + " next_scene_id='" + btn_id + "' "
+                + " onclick='onclick_next_scene_btn(this);'>"
+                + escapeHtml(btn_text) + "</div>";
+
+            /*
+            "next": [{
+                "text": "Show me what steam looks like",
+                "id": "steam_0"
+            }, {
+                "text": "Tell me something else",
+                "id": "show_0"
+            }]
+            */
+        }
+        // variants
+    }
+}
+
 function run_game() {
+    var startid = window.gameJson["options"]["start-scene"];
+    next_scene(startid);
     hide_screen_loading();
 }
